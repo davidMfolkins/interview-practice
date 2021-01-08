800f1e3fa6a192f7a6d1f827839d5429b02ee687
David Folkins <david.folkins88@gmail.com>
2021-01-08 11:06:17 -0500

    add comments

--- strings_and_arrays/reverseStr.js
+++ strings_and_arrays/reverseStr.js
@@ -1,4 +1,4 @@
-const str1 = "banana"
+const str1 = "hello"
 
 function reverseStr(str) {
   strReverse = "";
@@ -9,3 +9,13 @@ function reverseStr(str) {
 }
 
 reverseStr(str1)
+
+//loop through string starting at the end going backwards
+/* Here hello's length equals 5
+        For each iteration: i = str.length - 1 and newString = newString + str[i]
+        First iteration:    i = 5 - 1 = 4,         newString = "" + "o" = "o"
+        Second iteration:   i = 4 - 1 = 3,         newString = "o" + "l" = "ol"
+        Third iteration:    i = 3 - 1 = 2,         newString = "ol" + "l" = "oll"
+        Fourth iteration:   i = 2 - 1 = 1,         newString = "oll" + "e" = "olle"
+        Fifth iteration:    i = 1 - 1 = 0,         newString = "olle" + "h" = "olleh"
+    End of the FOR Loop*/
