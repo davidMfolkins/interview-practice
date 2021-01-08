f5d31c0fe62666cb4dbba19b3ba4ef9d5eac2944
David Folkins <david.folkins88@gmail.com>
2021-01-08 12:42:22 -0500

    add comments

--- strings_and_arrays/buyAndSell.js
+++ strings_and_arrays/buyAndSell.js
@@ -1,14 +1,19 @@
 //Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
 
-let maxProfit = function(prices) {
+let maxProfit = function (prices) {
   let profit = 0;
 
   for (let i = 1; i < prices.length; i++) {
     if (prices[i] > prices[i - 1]) {
       profit += prices[i] - prices[i - 1];
-    } 
+    }
   }
 
-  return profit;    
+  return profit;
 };
 
+
+//first you start the loop at the second index
+// check if the price of that index is greater than the price of the previous index
+//if true, add that to the total profit
+//if false continue loop
