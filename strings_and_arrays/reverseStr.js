481ce047bb09f80b459021a88f5cbb857f754d3e
David Folkins <david.folkins88@gmail.com>
2021-01-08 11:08:39 -0500

    add comments

--- strings_and_arrays/reverseStr.js
+++ strings_and_arrays/reverseStr.js
@@ -25,4 +25,9 @@ function reverseString(str) {
 }
 reverseString("hello");
 
-//
+//se the split() method to return a new array
+// ["h", "e", "l", "l", "o"]
+// Step 2. Use the reverse() method to reverse the new created array
+// ["o", "l", "l", "e", "h"]
+// Step 3. Use the join() method to join all elements of the array into a string
+// "olleh"
